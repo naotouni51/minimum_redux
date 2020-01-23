6955eff49994f5774c8513705654118cb5336bae
naotouni51 <naotouni51@gmail.com>
2020-01-24 08:05:19 +0900

    connectするまえの状態

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export default INCREMENT = "INCREMENT"
+export default DECREMENT = "DECREMENT"
+
+export default increment = () => ({
+  type: INCREMENT
+})
+
+export default decrement = () => ({
+  type: DECREMENT
+})
